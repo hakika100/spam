b0c1f8391c99c4641476be6da01ca9d83a0fd5e5
hakika100 <56877986+hakika100@users.noreply.github.com>
2019-12-16 17:58:11 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("640649880293081117")
+setInterval(function() {
+channel.send(`gegegedgeqg`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
